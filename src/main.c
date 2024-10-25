f3bbc5238439e7833101f55fa979e54ac19a7e60
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2024-10-25 17:03:43 -0300

    feat: paddle movement

--- src/main.c
+++ src/main.c
@@ -1,18 +1,36 @@
 #include "raylib.h"
 
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+#define PADDLE_WIDTH 10
+#define PADDLE_HEIGHT 70
+
+int player1Y, player2Y;
+
 int main(void)
 {
-  const int screenWidth = 800;
-  const int screenHeight = 450;
-
-  InitWindow(screenWidth, screenHeight, "pong");
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "pong");
   SetTargetFPS(60);
+
+  player1Y = player2Y = ((SCREEN_HEIGHT / 2) - (PADDLE_HEIGHT / 2));
 
   while (!WindowShouldClose())
   {
     BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("placeholder", 190, 200, 20, LIGHTGRAY);
+    ClearBackground(BLACK);
+
+    if (IsKeyDown(KEY_UP) && player2Y > 0)
+      player2Y -= 2;
+    if (IsKeyDown(KEY_DOWN) && player2Y < (SCREEN_HEIGHT - PADDLE_HEIGHT))
+      player2Y += 2;
+    if (IsKeyDown(KEY_W) && player1Y > 0)
+      player1Y -= 2;
+    if (IsKeyDown(KEY_S) && player1Y < (SCREEN_HEIGHT - PADDLE_HEIGHT))
+      player1Y += 2;
+
+    DrawRectangle(20, player1Y, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
+    DrawRectangle(770, player2Y, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
+
     EndDrawing();
   }
 
