fca5b83307c09dcc5c7fdcc0ac1820917c9593de
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2024-10-26 16:44:40 -0300

    refactor: modularize code

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int player1Y, player2Y;
 int ballX, ballY;
 int ballSpeedX, ballSpeedY;
 
-int main(void)
+void initialize()
 {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "pong");
   SetTargetFPS(60);
@@ -22,56 +22,79 @@ int main(void)
   ballX = SCREEN_WIDTH / 2;
   ballY = SCREEN_HEIGHT / 2;
   ballSpeedX = ballSpeedY = BALL_SPEED;
+}
+
+void draw()
+{
+  BeginDrawing();
+  ClearBackground(BLACK);
+
+  // Player 1 paddle
+  DrawRectangle(20, player1Y, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
+  // Player 2 paddle
+  DrawRectangle(SCREEN_WIDTH - 30, player2Y, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
+  // Ball
+  DrawRectangle(ballX, ballY, BALL_WIDTH, BALL_HEIGHT, WHITE);
+
+  EndDrawing();
+}
+
+void updatePaddleMovement()
+{
+  // Player 1 movement
+  if (IsKeyDown(KEY_W) && player1Y > 0)
+    player1Y -= PADDLE_SPEED;
+  if (IsKeyDown(KEY_S) && player1Y < (SCREEN_HEIGHT - PADDLE_HEIGHT))
+    player1Y += PADDLE_SPEED;
+
+  // Player 2 movement
+  if (IsKeyDown(KEY_UP) && player2Y > 0)
+    player2Y -= PADDLE_SPEED;
+  if (IsKeyDown(KEY_DOWN) && player2Y < (SCREEN_HEIGHT - PADDLE_HEIGHT))
+    player2Y += PADDLE_SPEED;
+}
+
+void updateBallMovement()
+{
+  // Check for collision with upper and bottom borders
+  if (ballY < 0 || ballY > (SCREEN_HEIGHT - 10))
+    ballSpeedY *= -1;
+
+  // Check if ball has reached left/right borders (teleport ball back to center)
+  if (ballX < 0 || ballX > (SCREEN_WIDTH - 10))
+  {
+    ballX = SCREEN_WIDTH / 2;
+    ballY = SCREEN_HEIGHT / 2;
+    ballSpeedX *= -1;
+    ballSpeedY *= -1;
+  }
+
+  // Check for collision with player 1
+  if (ballX < (20 + PADDLE_WIDTH) && (ballY > player1Y && ballY < (player1Y + PADDLE_HEIGHT)))
+  {
+    ballSpeedX *= -1;
+    ballSpeedY *= -1;
+  }
+  // Check for collision with player 2
+  if (ballX > (SCREEN_WIDTH - 30 - PADDLE_WIDTH) && (ballY > player2Y && ballY < (player2Y + PADDLE_HEIGHT)))
+  {
+    ballSpeedX *= -1;
+    ballSpeedY *= -1;
+  }
+
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+}
+
+int main(void)
+{
+  initialize();
 
   while (!WindowShouldClose())
   {
-    BeginDrawing();
-    ClearBackground(BLACK);
-
-    if (IsKeyDown(KEY_UP) && player2Y > 0)
-      player2Y -= PADDLE_SPEED;
-    if (IsKeyDown(KEY_DOWN) && player2Y < (SCREEN_HEIGHT - PADDLE_HEIGHT))
-      player2Y += PADDLE_SPEED;
-    if (IsKeyDown(KEY_W) && player1Y > 0)
-      player1Y -= PADDLE_SPEED;
-    if (IsKeyDown(KEY_S) && player1Y < (SCREEN_HEIGHT - PADDLE_HEIGHT))
-      player1Y += PADDLE_SPEED;
-
-    DrawRectangle(20, player1Y, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
-    DrawRectangle(SCREEN_WIDTH - 30, player2Y, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
-
-    DrawRectangle(ballX, ballY, BALL_WIDTH, BALL_HEIGHT, WHITE);
-
-    // Check for collision with upper and bottom borders
-    if (ballY < 0 || ballY > (SCREEN_HEIGHT - 10))
-      ballSpeedY *= -1;
-
-    // Check if ball has reached left/right borders (teleport ball back to center)
-    if (ballX < 0 || ballX > (SCREEN_WIDTH - 10))
-    {
-      ballX = SCREEN_WIDTH / 2;
-      ballY = SCREEN_HEIGHT / 2;
-      ballSpeedX *= -1;
-      ballSpeedY *= -1;
-    }
-
-    // Check for collision with player 1
-    if (ballX < (20 + PADDLE_WIDTH) && (ballY > player1Y && ballY < (player1Y + PADDLE_HEIGHT)))
-    {
-      ballSpeedX *= -1;
-      ballSpeedY *= -1;
-    }
-    // Check for collision with player 2
-    if (ballX > (SCREEN_WIDTH - 30 - PADDLE_WIDTH) && (ballY > player2Y && ballY < (player2Y + PADDLE_HEIGHT)))
-    {
-      ballSpeedX *= -1;
-      ballSpeedY *= -1;
-    }
-
-    ballX += ballSpeedX;
-    ballY += ballSpeedY;
-
-    EndDrawing();
+    updatePaddleMovement();
+    updateBallMovement();
+    draw();
   }
 
   CloseWindow();
